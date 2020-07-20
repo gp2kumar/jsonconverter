ee5a38d6151f3ebc0543cf5f44c335b81f885d2c
prgarre <garrepradeepkumar@gmail.com>
2020-07-20 18:27:34 +0530

    increased version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jsonConverter",
-    version="1.0.0",
+    version="1.0.1",
     author="prgarre",
     author_email="garrepradeepkumar@gmail.com",
     description="Json converter",
